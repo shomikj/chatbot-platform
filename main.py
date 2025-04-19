1a0c27f669211e82b060ce637ed0460f4f8d2cea
shomikj <shomikja@gmail.com>
2025-04-19 10:55:10 -0400

    auth

--- main.py
+++ main.py
@@ -1,12 +1,64 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request
 import gradio as gr
 import uvicorn
+from starlette.middleware.sessions import SessionMiddleware
+from starlette.config import Config
+from authlib.integrations.starlette_client import OAuth, OAuthError
+import os
+from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
+
+config_data = {'GOOGLE_CLIENT_ID': GOOGLE_CLIENT_ID, 'GOOGLE_CLIENT_SECRET': GOOGLE_CLIENT_SECRET}
+starlette_config = Config(environ=config_data)
+oauth = OAuth(starlette_config)
+oauth.register(
+    name='google',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'},
+)
+app.add_middleware(SessionMiddleware, secret_key="my_secret")
+
+def get_user(request: Request):
+    user = request.session.get('user')
+    if user:
+        return user['email']
+    return None
+
+@app.get('/')
+def public(user: dict = Depends(get_user)):
+    if user:
+        return RedirectResponse(url='/chat')
+    else:
+        return RedirectResponse(url='/login-page')
+
+@app.route('/logout')
+async def logout(request: Request):
+    request.session.pop('user', None)
+    return RedirectResponse(url='/')
+
+@app.route('/login')
+async def login(request: Request):
+    redirect_uri = request.url_for('auth')
+    #redirect_uri = urlunparse(urlparse(str(redirect_uri))._replace(scheme='https'))
+    return await oauth.google.authorize_redirect(request, redirect_uri)
+
+@app.route('/auth')
+async def auth(request: Request):
+    try:
+        access_token = await oauth.google.authorize_access_token(request)
+    except OAuthError:
+        return RedirectResponse(url='/')
+    request.session['user'] = dict(access_token)["userinfo"]
+    return RedirectResponse(url='/')
+
+with gr.Blocks() as login_demo:
+    gr.Button("Login", link="/login")
+
+app = gr.mount_gradio_app(app, login_demo, path="/login-page")
 
 def alternatingly_agree(message, history):
     if len([h for h in history if h['role'] == "assistant"]) % 2 == 0:
